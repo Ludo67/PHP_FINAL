95f3b007a36443b26f64d96d356540bdb3e8feda
LawrenceLiang02 <54686978+LawrenceLiang02@users.noreply.github.com>
2021-05-17 15:50:56 -0400

    Update tbl_product.sql

--- tbl_product.sql
+++ tbl_product.sql
@@ -1,23 +1,24 @@
 -- phpMyAdmin SQL Dump
--- version 4.0.10.18
--- https://www.phpmyadmin.net
+-- version 5.0.4
+-- https://www.phpmyadmin.net/
 --
--- Host: localhost:3306
--- Generation Time: Mar 05, 2018 at 03:20 AM
--- Server version: 5.6.36-cll-lve
--- PHP Version: 5.6.30
+-- Host: 127.0.0.1
+-- Generation Time: May 17, 2021 at 09:45 PM
+-- Server version: 10.4.17-MariaDB
+-- PHP Version: 8.0.1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `testing`
+-- Database: `phpfinalproject_shoppingcart`
 --
 
 -- --------------------------------------------------------
@@ -26,22 +27,44 @@ SET time_zone = "+00:00";
 -- Table structure for table `tbl_product`
 --
 
-CREATE TABLE IF NOT EXISTS `tbl_product` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `tbl_product` (
+  `id` int(11) NOT NULL,
   `name` varchar(255) NOT NULL,
   `image` varchar(255) NOT NULL,
-  `price` double(10,2) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=10 ;
+  `price` double(10,2) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `tbl_product`
 --
 
 INSERT INTO `tbl_product` (`id`, `name`, `image`, `price`) VALUES
-(1, 'Samsung J2 Pro', '1.jpg', 100.00),
-(2, 'HP Notebook', '2.jpg', 299.00),
-(3, 'Panasonic T44 Lite', '3.jpg', 125.00);
+(15, 'Super Mario Party', '5.jpg', 79.99),
+(14, 'Mario Kart 8 Deluxe', '4.jpg', 79.99),
+(13, 'Pikmin 3Deluxe', '3pikmin.jpg', 72.99),
+(12, 'Animal Crossing: New Horizons', '2animalCrossing.jpg', 69.99),
+(11, 'Luigi\'s Mansion 3', '1Luigi.jpg', 79.99);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tbl_product`
+--
+ALTER TABLE `tbl_product`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tbl_product`
+--
+ALTER TABLE `tbl_product`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
